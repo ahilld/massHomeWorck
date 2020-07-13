399d6ff3fc85d9894bd4b8576d1433306a96cb1e
Sergey Deshko <axilhik@gmail.com>
2020-07-13 13:17:29 +0300

    add task 1
    add index.js with task1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
 
+/**
+ * @deprecated - Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
+
+ */
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arrRes = arr1.concat(arr2);
+
+console.log(arrRes);
+
+
