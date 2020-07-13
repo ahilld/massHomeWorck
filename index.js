a78b446453c459326d140fb370094fd4b618d688
Sergey Deshko <axilhik@gmail.com>
2020-07-13 13:19:10 +0300

    add task 2
    add index.js with task 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- * @deprecated - Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
+ * @description - Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
 
  */
 
@@ -11,4 +11,12 @@ const arrRes = arr1.concat(arr2);
 
 console.log(arrRes);
 
+/**
+ * @description - Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+ *
+ */
 
+const array = [1,2,3]
+const reversed = array.reverse()
+console.log(reversed);
+console.log(array);
